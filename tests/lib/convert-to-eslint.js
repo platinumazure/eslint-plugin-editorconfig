99910441bd7c509dda92824b7608d7909b8cdb36
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2017-02-25 13:20:39 -0600

    New: indent rule, derived from indent_size and indent_style

--- tests/lib/convert-to-eslint.js
+++ tests/lib/convert-to-eslint.js
@@ -8,7 +8,103 @@ const assert = require("chai").assert;
 const convertToESLint = require("../../lib/convert-to-eslint");
 
 describe("convert-to-eslint", function() {
-    it("sanity test", function() {
-        assert.ok(convertToESLint);
+    describe("indent", function() {
+        describe("2 spaces", function() {
+            beforeEach(function() {
+                this.result = convertToESLint({
+                    indent_size: 2,
+                    indent_style: "space"
+                });
+            });
+
+            it("should result in indent: [\"error\", 2]", function() {
+                assert.deepEqual(this.result, {
+                    indent: ["error", 2]
+                });
+            });
+        });
+
+        describe("4 spaces", function() {
+            beforeEach(function() {
+                this.result = convertToESLint({
+                    indent_size: 4,
+                    indent_style: "space"
+                });
+            });
+
+            it("should result in indent: [\"error\", 4]", function() {
+                assert.deepEqual(this.result, {
+                    indent: ["error", 4]
+                });
+            });
+        });
+
+        describe("Tabs (width: 2)", function() {
+            beforeEach(function() {
+                this.result = convertToESLint({
+                    indent_size: 2,
+                    indent_style: "tab"
+                });
+            });
+
+            it("should result in indent: [\"error\", \"tab\"]", function() {
+                assert.deepEqual(this.result, {
+                    indent: ["error", "tab"]
+                });
+            });
+        });
+
+        describe("Tabs (width: 4)", function() {
+            beforeEach(function() {
+                this.result = convertToESLint({
+                    indent_size: 4,
+                    indent_style: "tab"
+                });
+            });
+
+            it("should result in indent: [\"error\", \"tab\"]", function() {
+                assert.deepEqual(this.result, {
+                    indent: ["error", "tab"]
+                });
+            });
+        });
+
+        describe("Missing indent_style", function() {
+            beforeEach(function() {
+                this.result = convertToESLint({
+                    indent_size: 4
+                });
+            });
+
+            it("should result in no indent rule", function() {
+                assert.notProperty(this.result, "indent");
+            });
+        });
+
+        describe("Missing indent_size, indent_style is space", function() {
+            beforeEach(function() {
+                this.result = convertToESLint({
+                    indent_style: "space"
+                });
+            });
+
+            it("should result in no indent rule", function() {
+                assert.notProperty(this.result, "indent");
+            });
+        });
+
+        describe("Missing indent_size, indent_style is tab", function() {
+            beforeEach(function() {
+                this.result = convertToESLint({
+                    indent_style: "tab"
+                });
+            });
+
+            it("should result in indent: [\"error\", \"tab\"]", function() {
+                assert.deepEqual(this.result, {
+                    indent: ["error", "tab"]
+                });
+            });
+        });
     });
 });
